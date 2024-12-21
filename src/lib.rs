3bef2eb40ff8965daef7c669c867468602c086b9
Ian McIntyre <me@mciantyre.dev>
2024-12-21 10:37:44 -0500

    Remove Element, buffers

--- src/lib.rs
+++ src/lib.rs
@@ -7,16 +7,12 @@ use core::{
     task::{Context, Poll},
 };
 
-pub trait Element: Copy {}
-impl Element for u8 {}
-
-pub trait Destination<E: Element> {}
+pub trait Destination<E> {}
 pub struct Channel<const DMA_INST: u8>();
 
 pub struct Write<'a, D, E, const DMA_INST: u8>
 where
     D: Destination<E>,
-    E: Element,
 {
     _channel: &'a Channel<DMA_INST>,
     _destination: &'a mut D,
@@ -26,7 +22,6 @@ where
 impl<D, E, const DMA_INST: u8> Future for Write<'_, D, E, DMA_INST>
 where
     D: Destination<E>,
-    E: Element,
 {
     type Output = ();
     fn poll(self: Pin<&mut Self>, _: &mut Context<'_>) -> Poll<Self::Output> {
@@ -36,12 +31,10 @@ where
 
 pub fn write<'a, D, E, const DMA_INST: u8>(
     _channel: &'a mut Channel<DMA_INST>,
-    _: &'a [E],
     _destination: &'a mut D,
 ) -> Write<'a, D, E, DMA_INST>
 where
     D: Destination<E>,
-    E: Element,
 {
     Write {
         _channel,
@@ -57,10 +50,8 @@ impl<P, const N: u8> Periph<P, N> {
     pub fn dma_write<'a, const DMA_INST: u8>(
         &'a mut self,
         channel: &'a mut Channel<DMA_INST>,
-        buffer: &'a [u8],
     ) -> Write<'a, Self, u8, DMA_INST>
-    where
-    {
-        write(channel, buffer, self)
+where {
+        write(channel, self)
     }
 }
