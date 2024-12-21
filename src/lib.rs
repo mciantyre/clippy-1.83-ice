863317d1d9bd4875dbfc8faabc118923a16b63c5
Ian McIntyre <me@mciantyre.dev>
2024-12-21 11:05:31 -0500

    Remove the destination peripheral

--- src/lib.rs
+++ src/lib.rs
@@ -9,26 +9,23 @@ use core::{
 
 pub struct Channel<const DMA_INST: u8>();
 
-pub struct Write<'a, D, E, const DMA_INST: u8> {
+pub struct Write<'a, E, const DMA_INST: u8> {
     _channel: &'a Channel<DMA_INST>,
-    _destination: &'a mut D,
     _elem: PhantomData<&'a E>,
 }
 
-impl<D, E, const DMA_INST: u8> Future for Write<'_, D, E, DMA_INST> {
+impl<E, const DMA_INST: u8> Future for Write<'_, E, DMA_INST> {
     type Output = ();
     fn poll(self: Pin<&mut Self>, _: &mut Context<'_>) -> Poll<Self::Output> {
         Poll::Ready(())
     }
 }
 
-pub fn write<'a, D, E, const DMA_INST: u8>(
+pub fn write<'a, E, const DMA_INST: u8>(
     _channel: &'a mut Channel<DMA_INST>,
-    _destination: &'a mut D,
-) -> Write<'a, D, E, DMA_INST> {
+) -> Write<'a, E, DMA_INST> {
     Write {
         _channel,
-        _destination,
         _elem: PhantomData,
     }
 }
@@ -39,7 +36,7 @@ impl<P, const N: u8> Periph<P, N> {
     pub fn dma_write<'a, const DMA_INST: u8>(
         &'a mut self,
         channel: &'a mut Channel<DMA_INST>,
-    ) -> Write<'a, Self, u8, DMA_INST> {
-        write(channel, self)
+    ) -> Write<'a, u8, DMA_INST> {
+        write(channel)
     }
 }
